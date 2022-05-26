ea15ff42a397b1d7f1f8111d9b1b92c14701a1da
Torbjorn Tornkvist <kruskakli@gmail.com>
2022-05-26 10:25:29 +0200

    Removing panic from args processing, producing better error output

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
-use std::env;
+use std::{env, process};
 
 mod ui;
 
 fn main() -> anyhow::Result<()> {
-    let filename = parse_args();
+
+let filename = parse_args()
+    .unwrap_or_else(|err| {
+        println!("Wrong input arguments: {}", err);
+        process::exit(1);
+    });
 
     let app = ui::App::new(filename)?;
     let res = app.run();
@@ -12,11 +17,11 @@ fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-fn parse_args() -> String {
+fn parse_args() -> Result<String, &'static str> {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        panic!("no filename specified");
+        return Err("no filename specified");
     }
     let filename = &args[1];
-    filename.to_string()
+    Ok(filename.to_string())
 }
