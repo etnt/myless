ea31e1edea6b97a14fadf9487c781b2a7becbc71
Torbjorn Tornkvist <kruskakli@gmail.com>
2022-05-26 09:42:19 +0200

    Cleanup parsing the args

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,20 @@ use std::env;
 mod ui;
 
 fn main() -> anyhow::Result<()> {
+    let filename = parse_args();
+
+    let app = ui::App::new(filename)?;
+    let res = app.run();
+    println!("res = {:?}", res);
+
+    Ok(())
+}
+
+fn parse_args() -> String {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         panic!("no filename specified");
     }
     let filename = &args[1];
-
-    let app = ui::App::new(filename.clone())?;
-    let res = app.run();
-    println!("res = {:?}", res);
-    Ok(())
+    filename.to_string()
 }
